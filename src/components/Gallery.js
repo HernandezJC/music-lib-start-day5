fd6aef6881df8ec0da623c8e9e317024558e0ad9
HernandezJC <jchernandez1021@gmail.com>
2023-07-10 04:04:17 -0700

    updated music library

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,8 +1,12 @@
+import { useContext } from 'react'
+import { DataContext } from '../context/DataContext'
 import GalleryItem from './GalleryItem'
 
 function Gallery(props){
+    const data = useContext(DataContext)
+    console.log('DATA from CONTEXT in gallery file', data)
 
-    const display = props.data.map((item, index) => {
+    const display = data.map((item, index) => {
         return (
             <GalleryItem item={item} key={index} />
         )
